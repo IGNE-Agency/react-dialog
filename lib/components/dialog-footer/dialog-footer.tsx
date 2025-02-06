6badbac47a53e2bf299f6c395f554b819c1f59ed
Wouter Lem <wouter@igne.nl>
2025-02-06 14:06:37 +0100

    Fix layout

--- lib/components/dialog-footer/dialog-footer.tsx
+++ lib/components/dialog-footer/dialog-footer.tsx
@@ -1,4 +1,6 @@
+import classNames from "classnames";
 import type { ReactNode } from "react";
+import style from "./dialog-footer.module.css";
 
 export type DialogFooterProps = Readonly<{
 	children?: ReactNode;
@@ -9,7 +11,10 @@ const DialogFooter = ({
 	children,
 	className
 }: DialogFooterProps) => (
-	<footer className={className}>{children}</footer>
+	<footer
+		className={classNames([style.footer, className])}>
+		{children}
+	</footer>
 );
 
 export default DialogFooter;
--- lib/components/dialog-header/dialog-header.tsx
+++ lib/components/dialog-header/dialog-header.tsx
@@ -1,11 +1,20 @@
+import classNames from "classnames";
 import type { ReactNode } from "react";
+import style from "./dialog-header.module.css";
 
 export type DialogHeaderProps = Readonly<{
 	children?: ReactNode;
+	className?: string;
 }>;
 
 const DialogHeader = ({
-	children
-}: DialogHeaderProps) => <header>{children}</header>;
+	children,
+	className
+}: DialogHeaderProps) => (
+	<header
+		className={classNames([style.header, className])}>
+		{children}
+	</header>
+);
 
 export default DialogHeader;
